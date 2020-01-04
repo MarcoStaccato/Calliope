272cecacd49791e42f72aec550f853d68d3e7c8f
Marco Staccato <marcostaccato@gmail.com>
2020-01-03 23:28:22 -0600

    feat: use while loop to reverse array in place

--- src/main/java/com/staccato/cake/arrays/ReverseArray.java
+++ src/main/java/com/staccato/cake/arrays/ReverseArray.java
@@ -8,16 +8,30 @@ public class ReverseArray {
 
         String test = "abcde";
         System.out.println(reverse(test));
-        System.out.println(test.length());
+
+        System.out.println(reverseV2("test for reversing with while"));
     }
 
     public static String reverse(String input){
         char[] inputArr = input.toCharArray();
-        System.out.println(inputArr.length);
         for(int i=0; i<inputArr.length/2; i++){
             char current = inputArr[i];
             inputArr[i] = inputArr[inputArr.length - i - 1];
             inputArr[inputArr.length - i - 1] = current;
+        }
+        return new String(inputArr);
+    }
+
+    public static String reverseV2(String input){
+        char[] inputArr = input.toCharArray();
+        int start = 0;
+        int end = input.length()-1;
+        while(start < end){
+            char current = inputArr[start];
+            inputArr[start] = inputArr[end];
+            inputArr[end] = current;
+            start++;
+            end--;
         }
         return new String(inputArr);
     }
